938147618992c5e7fc4a3805e98e67422e99aacd
Linnéa Sjöström <ls8495@ad.lystkom.se>
2021-04-12 11:53:15 +0200

    Working on Tic-Tac-Toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,6 +8,7 @@ player = "O"
 
 game = True
 
+winner = None
 
 def showboard():
     print(" ___ ___ ___") 
@@ -20,27 +21,13 @@ def showboard():
         
 
 def playgame():
+    showboard()
     while game:
         turn()
+        gameover()
+        switchplayer()
     
 def turn():
-    '''for i in range(0,9):
-            switchplayer()
-            count = 0
-            showboard()
-            orientation = int(input("Välj en plats mellan 1-9: ")) #check for smaller or bigger than 1 and 9
-            orientation = orientation - 1
-            if board[orientation] == ' ':
-                board[orientation] = player
-                count += 1
-            else:
-                while board[orientation] != ' ':
-                    orientation = int(input("Tyvärr är platsen upptagen, välj en annan: "))
-                orientation = orientation - 1
-                board[orientation] = player
-    else:
-        showboard()    '''
-    switchplayer()
     count = 0
     showboard()
     orientation = int(input("Välj en plats mellan 1-9: ")) #check for smaller or bigger than 1 and 9
@@ -62,42 +49,114 @@ def switchplayer():
     elif player == "O":
         player = "X"
     return
+
+def gameover():
+    winner()
+    tie()
         
         
 def winner():
-    checkrow()
-    checkcolumn()
-    checkdiagonal()
+    global winner
+    rowwinner = checkrow()
+    columnwinner = checkcolumn()
+    diagonalwinner = checkdiagonal()
+    # Get the winner
+    if rowwinner:
+        winner = rowwinner
+    elif columnwinner:
+        winner = columnwinner
+    elif diagonalwinner:
+        winner = diagonal_winner
+    else:
+        winner = None    
+            
 
-def checkrow(): '''FÅ DETTA ATT FUNGERA?'''
+def checkrow():
     row1 = board[0] == board[1] == board[2] != ""
     row2 = board[3] == board[4] == board[5] != ""
     row3 = board[6] == board[7] == board[8] != ""
     
-    if row_1 or row_2 or row_3:
+    if row1 or row2 or row3:
         global game
         game = False
-    if row_1:
+    if row1:
         return board[0] 
-    elif row_2:
+    elif row2:
         return board[3] 
-    elif row_3:
+    elif row3:
         return board[6] 
       # Or return None if there was no winner
     else:
         return None    
     
 def checkcolumn():
-    return
+    column1 = board[0] == board[3] == board[6] != ""
+    column2 = board[1] == board[4] == board[7] != ""
+    column3 = board[2] == board[5] == board[8] != ""
+    
+    if column1 or column2 or column3:
+        global game
+        game = False
+    if column1:
+        return board[0] 
+    elif column2:
+        return board[1] 
+    elif column3:
+        return board[2] 
+      # Or return None if there was no winner
+    else:
+        return None    
     
 def checkdiagonal():
-    return
+    diagonal1 = board[0] == board[5] == board[8] != ""
+    diagonal2 = board[2] == board[5] == board[7] != ""
+    
+    if diagonal1 or diagonal2:
+        global game
+        game = False
+    if diagonal1:
+        return board[0] 
+    elif diagonal2:
+        return board[2] 
+      # Or return None if there was no winner
+    else:
+        return None     
         
 #def checkwins = rows, column and diagonal.
 
+def tie():
+    # Set global variables
+    global game
+    # If board is full
+    if "-" not in board:
+        game = False
+        return True
+    # Else there is no tie
+    else:
+        return False
+
 
 def runningcode():
+    print("Detta är din spelplan:")
     playgame()
-    winner()
+        
 
 runningcode()
+
+''' Skit
+for i in range(0,9):
+            switchplayer()
+            count = 0
+            showboard()
+            orientation = int(input("Välj en plats mellan 1-9: ")) #check for smaller or bigger than 1 and 9
+            orientation = orientation - 1
+            if board[orientation] == ' ':
+                board[orientation] = player
+                count += 1
+            else:
+                while board[orientation] != ' ':
+                    orientation = int(input("Tyvärr är platsen upptagen, välj en annan: "))
+                orientation = orientation - 1
+                board[orientation] = player
+    else:
+        showboard()    '''
